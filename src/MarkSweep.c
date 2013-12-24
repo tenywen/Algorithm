5c48d9d5ac02c0a201536420f76315ec7bff905d
tenywen <liupeng@nibirutech.com>
2013-12-24 00:53:08 -0800

    update

--- src/MarkSweep.c
+++ src/MarkSweep.c
@@ -6,6 +6,7 @@
 *       @
 *       @
 */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -20,23 +21,29 @@ typedef int bool;
 
 typedef int error;
 enum TYPE {
-    Int     = 1,
-    Pair    = 2,
+    INT     = 1,
+    STRUCT  = 2,
+    POINT   = 3,    // 指针&引用
 } ;
 
 struct Obj {
-    enum TYPE type;
+    enum TYPE Type;
     union {
         int int_value;
-        struct pair* pair_value;
+        struct point* point_value;
     } Value;
-    struct Obj* next;
+    struct Obj* Next;
+    int Count;  // 引用计数器
     bool Mark;
 };
 
 struct pair {
     struct Obj* head;
     struct Obj* tail; 
+};
+
+struct point {
+    struct Obj* value;
 };
 
 struct VM {
@@ -48,6 +55,23 @@ struct VM {
 int InitObj(struct Obj* obj) {
     // undo
     return 0;
+}
+
+int ObjCountAdd(struct Obj* obj) {
+    obj->Count ++;
+    return 0;
+}
+
+struct Obj* Malloc(struct VM* vm,TYPE type) {
+    struct Obj* obj = (struct Obj*)malloc(sizeof(struct Obj));
+    printf("malloc %p",obj);
+    if(!obj) 
+        return NULL; 
+    InitObj(obj);
+    obj->Type = type;
+    MarkObj(obj);
+    pushVM(vm,obj);
+    return obj;
 }
 
 struct VM* newVM() {
@@ -62,6 +86,20 @@ bool pushVM(struct VM* vm,struct Obj* obj) {
         return false;
     vm->objs[vm->length ++] = obj; 
     return true;
+}
+
+// 限于指针绑定
+int Bind(struct Obj* obj, struct Obj* src) {
+    if(obj->Type != POINT) {
+        return 0;  
+    } 
+    struct Obj* real = src;
+    if(src->Type == POINT) {
+        real = src->point_value->value;
+    }
+    obj->point_value->value = real;
+    real->Count ++;
+    return 0;
 }
 
 
@@ -86,13 +124,17 @@ int Mark(struct VM* vm) {
     return 0;
 }
 
+int Free(struct Obj* obj) {
+    if(obj->)
+}
+
 int Sweep(struct VM* vm) {
     struct Obj** curr;
     struct Obj* entry;
     curr = &vm->head;
     for(; *curr;) {
         entry = *curr;
-        if(!entry->Mark) {
+        if(!entry->Count) {
             *curr = entry->next;  
             free(entry); 
         } else {
@@ -101,16 +143,6 @@ int Sweep(struct VM* vm) {
         }
     }
     return 0;
-}
-
-struct Obj* Malloc(struct VM* vm) {
-    struct Obj* obj = (struct Obj*)malloc(sizeof(struct Obj));
-    if(!obj) 
-        return NULL; 
-    InitObj(obj);
-    pushVM(vm,obj);
-    MarkObj(obj);
-    return obj;
 }
 
 int main() {
